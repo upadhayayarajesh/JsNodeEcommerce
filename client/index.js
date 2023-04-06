207322026c39d35da2e6c4628d2e98f7280d3166
rajesh Upadhayaya <rajeshUpadhayaya68@gmail.com>
2023-04-05 19:20:45 -0600

    create a login page and other direcotries.

---
+++ client/index.js
@@ -0,0 +1,30 @@
+const express =require('express');
+const path=require('path')
+require("dotenv").config();
+
+
+const app= express();
+
+
+
+// using ejs as an engine and use view engine as html.
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static("public"))
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+//middlewares
+app.use(express.urlencoded({extended: true}));
+
+
+app.use(require("./routes/root"))
+
+/*app.get('/',async(req, res) =>{
+    res.render("./indexFile/index")
+});*/
+
+
+
+//Server running on 3000.
+app.listen(3000, () => console.log(`Client started listening on port: ${process.env.PORT}`));
